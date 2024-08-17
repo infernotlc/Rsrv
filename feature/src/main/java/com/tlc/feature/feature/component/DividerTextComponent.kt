70dd534a807e1051fc5b9856690f35b2cf38ae84
Talha Çalışır <70065773+infernotlc@users.noreply.github.com>
2024-08-17 23:59:34 +0300

    feat:added

---
+++ feature/src/main/java/com/tlc/feature/feature/component/DividerTextComponent.kt
@@ -0,0 +1,39 @@
+package com.tlc.feature.feature.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun DividerTextComponent(textColor: Color = Color.LightGray) {
+    Row(modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically) {
+
+        HorizontalDivider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            thickness = 1.dp, color = Color.Black
+        )
+
+        Text(modifier = Modifier.padding(8.dp),text = "or",fontSize = 14.sp, color = textColor)
+
+        HorizontalDivider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            thickness = 1.dp, color = Color.Black
+        )
+
+
+
+    }
+}
