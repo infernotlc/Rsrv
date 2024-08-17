b585ba75d1e380588452bafe8af81752238fec95
Talha Çalışır <70065773+infernotlc@users.noreply.github.com>
2024-08-17 23:52:56 +0300

    feat:added

---
+++ feature/src/main/java/com/tlc/feature/feature/auth/login/state/LoginUIState.kt
@@ -0,0 +1,10 @@
+package com.tlc.feature.feature.auth.login.state
+
+import com.google.firebase.auth.FirebaseUser
+
+data class LoginUiState(
+    val isLoading: Boolean = false,
+    val user: FirebaseUser? = null,
+    val error: String? = null,
+    val isLoggedInAnonymously: Boolean = false
+)
