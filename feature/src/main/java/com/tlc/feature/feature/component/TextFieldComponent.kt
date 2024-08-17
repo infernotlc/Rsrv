13461716bd7e6df2318dad158b86c4e8ed09f0ba
Talha Çalışır <70065773+infernotlc@users.noreply.github.com>
2024-08-17 23:41:07 +0300

    added

---
+++ feature/src/main/java/com/tlc/feature/feature/component/TextFieldComponent.kt
@@ -0,0 +1,58 @@
+package com.tlc.feature.feature.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TextFieldComponent(
+    stateValue: String,
+    label: String,
+    onValueChange: (String) -> Unit,
+    painterResource: Painter,
+    focusedLabelColor: Color = Color.Gray,
+    unfocusedLabelColor: Color = Color.LightGray,
+    focusedBorderColor: Color = Color.Gray,
+    unfocusedBorderColor: Color = Color.LightGray
+) {
+    var textValue by remember { mutableStateOf(stateValue) }
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(4.dp)),
+        label = { Text(text = label) },
+        value = textValue,
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedLabelColor = focusedLabelColor,
+            unfocusedLabelColor = unfocusedLabelColor,
+            focusedBorderColor = focusedBorderColor,
+            unfocusedBorderColor = unfocusedBorderColor,
+            focusedContainerColor = Color.White,
+            unfocusedContainerColor = Color.White
+        ),
+        onValueChange = {
+            textValue = it
+            onValueChange(it)
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        leadingIcon = {
+            Icon(painter = painterResource, contentDescription = "")
+        }
+    )
+}
