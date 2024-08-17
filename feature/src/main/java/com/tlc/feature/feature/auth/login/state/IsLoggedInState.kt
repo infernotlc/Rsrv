fe6518782aa0093bb1303679dae1f35848e26ccd
Talha Çalışır <70065773+infernotlc@users.noreply.github.com>
2024-08-17 23:55:36 +0300

    feat:added

---
+++ feature/src/main/java/com/tlc/feature/feature/auth/login/state/IsLoggedInState.kt
@@ -0,0 +1,8 @@
+package com.tlc.feature.feature.auth.login.state
+
+data class IsLoggedInState(
+    val transaction: Boolean = false,
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val isAnonymous: Boolean = false
+)
