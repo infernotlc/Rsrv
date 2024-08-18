6f8d534963ac36b8d414610d37f8592aebbdd37b
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-18 23:52:13 +0300

    feat:added

---
+++ feature/src/main/java/com/tlc/feature/navigation/main_datastore/MainDataStore.kt
@@ -0,0 +1,60 @@
+package com.tlc.feature.navigation.main_datastore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+import javax.inject.Inject
+import javax.inject.Singleton
+
+private val Context.myPreferencesDataStore: DataStore<Preferences> by preferencesDataStore("settings")
+
+@Singleton
+class MainDataStore @Inject constructor(
+    @ApplicationContext context: Context
+) {
+    private val myPreferencesDataStore = context.myPreferencesDataStore
+
+    private object PreferencesKeys {
+        val APP_ENTRY_KEY = booleanPreferencesKey("app_entry")
+        val GALLERY_PERMISSION_KEY = booleanPreferencesKey("gallery_permission")
+    }
+
+    val readGalleryPermission = myPreferencesDataStore.data.catch { exception ->
+        if (exception is IOException) {
+            emit(emptyPreferences())
+        } else {
+            throw exception
+        }
+    }.map { preferences ->
+        preferences[PreferencesKeys.GALLERY_PERMISSION_KEY] ?: false
+    }
+
+    suspend fun saveGalleryPermission(granted: Boolean) {
+        myPreferencesDataStore.edit { preferences ->
+            preferences[PreferencesKeys.GALLERY_PERMISSION_KEY] = granted
+        }
+    }
+    val readAppEntry = myPreferencesDataStore.data.catch { exception ->
+        if (exception is IOException) {
+            emit(emptyPreferences())
+        } else {
+            throw exception
+        }
+    }.map { preferences ->
+        preferences[PreferencesKeys.APP_ENTRY_KEY] ?: true
+    }
+
+    suspend fun saveAppEntry() {
+        myPreferencesDataStore.edit { preferences ->
+            preferences[PreferencesKeys.APP_ENTRY_KEY] = false
+        }
+    }
+}
