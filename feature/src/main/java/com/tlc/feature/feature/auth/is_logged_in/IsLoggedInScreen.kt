a5c8288d88dab5676a290ece107acb7188f127f6
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-18 23:55:17 +0300

    feat:user can navigate to screen as his role

---
+++ feature/src/main/java/com/tlc/feature/feature/auth/is_logged_in/IsLoggedInScreen.kt
@@ -0,0 +1,64 @@
+package com.tlc.feature.feature.auth.is_logged_in
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.tlc.domain.utils.RootResult
+import com.tlc.feature.R
+import com.tlc.feature.feature.auth.login.viewmodel.LoginViewModel
+import com.tlc.feature.feature.component.LoadingLottie
+import com.tlc.feature.navigation.NavigationGraph
+
+@Composable
+fun IsLoggedIn(navController: NavController, viewModel: LoginViewModel = hiltViewModel()){
+
+    val loggingState by viewModel.loggingState.collectAsState()
+
+    LaunchedEffect(true) {
+        viewModel.isLoggedIn()
+    }
+
+    LaunchedEffect(loggingState.transaction) {
+        if (loggingState.transaction) {
+            when (loggingState.data) {
+                "admin" -> navController.navigate(NavigationGraph.ADMIN_SCREEN.route) {
+                    popUpTo(NavigationGraph.ADMIN_SCREEN.route) {
+                        inclusive = true
+                    }
+                }
+                "customer" -> navController.navigate(NavigationGraph.CUSTOMER_SCREEN.route) {
+                    popUpTo(NavigationGraph.CUSTOMER_SCREEN.route) {
+                        inclusive = true
+                    }
+                }
+                else -> {
+                    navController.navigate(NavigationGraph.LOGIN.route) {
+                        popUpTo(NavigationGraph.LOGIN.route) {
+                            inclusive = true
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    if (loggingState.isLoading){
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            LoadingLottie(R.raw.loading_lottie)
+        }
+    }
+
+}
