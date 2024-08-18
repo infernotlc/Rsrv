de83c522a24c033fd06d8138147ce5e5d41af588
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-18 23:57:27 +0300

    feat:added

---
+++ feature/src/main/java/com/tlc/feature/feature/admin/AdminScreen.kt
@@ -0,0 +1,16 @@
+package com.tlc.feature.feature.admin
+
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavHostController
+
+@Composable
+fun AdminScreen(
+    navController: NavHostController
+)
+{
+    Text(text = "Admin Screen")
+}
