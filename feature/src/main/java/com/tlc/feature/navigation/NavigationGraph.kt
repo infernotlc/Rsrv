2b8d0170e20ec5e43000a4955b03ff8b9a6f70f7
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-18 23:51:44 +0300

    feat:added

---
+++ feature/src/main/java/com/tlc/feature/navigation/NavigationGraph.kt
@@ -0,0 +1,10 @@
+package com.tlc.feature.navigation
+
+enum class NavigationGraph(val route: String) {
+    IS_LOGGED_IN("is_logged_in"),
+    LOGIN("login"),
+    REGISTER("register"),
+    FORGOT_PASSWORD("forgot_password"),
+    ADMIN_SCREEN("admin_screen"),
+    CUSTOMER_SCREEN("customer_screen");
+}
