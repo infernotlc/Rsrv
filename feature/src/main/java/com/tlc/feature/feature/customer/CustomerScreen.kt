f43a128acd72afc72e1b704bdad034b62fa95f6f
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-18 23:56:17 +0300

    feat:added

---
+++ feature/src/main/java/com/tlc/feature/feature/customer/CustomerScreen.kt
@@ -0,0 +1,12 @@
+package com.tlc.feature.feature.customer
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun CustomerScreen(
+    navController: NavController
+) {
+    Text(text = "Customer Screen")
+}
