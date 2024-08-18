e5e003c7fb2134a4ea396bd7cbae2bc9c0e52e22
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-18 23:53:07 +0300

    feat:edited

--- feature/src/main/java/com/tlc/feature/feature/auth/login/viewmodel/LoginViewModel.kt
+++ feature/src/main/java/com/tlc/feature/feature/auth/login/viewmodel/LoginViewModel.kt
@@ -4,6 +4,9 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.tlc.data.ui.repository.firebase.AuthRepositoryImpl
+import com.tlc.domain.repository.firebase.AuthRepository
 import com.tlc.domain.use_cases.firebase_use_cases.auth.IsLoggedInUseCase
 import com.tlc.domain.use_cases.firebase_use_cases.auth.SignInUseCase
 import com.tlc.domain.use_cases.firebase_use_cases.auth.SignOutUseCase
@@ -21,6 +24,7 @@ class LoginViewModel @Inject constructor(
     private val signInUseCase: SignInUseCase,
     private val signOutUseCase: SignOutUseCase,
     private val isLoggedInUseCase: IsLoggedInUseCase,
+    private val authRepository: AuthRepositoryImpl
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(LoginUiState())
@@ -29,34 +33,34 @@ class LoginViewModel @Inject constructor(
     private val _loggingState = MutableStateFlow(IsLoggedInState())
     val loggingState: StateFlow<IsLoggedInState> = _loggingState
 
+
     internal fun signIn(email: String, password: String) {
         viewModelScope.launch {
-
             _uiState.value = _uiState.value.copy(isLoading = true)
             signInUseCase(email, password).collect { result ->
                 when (result) {
                     is RootResult.Success -> {
-                        result.data?.let { user ->
-                            Log.d("LoginViewModel", "User: $user")
-                            _uiState.value = _uiState.value.copy(user = user, isLoading = false)
-                        }
+                        val user = result.data
+                        val role = user?.let { authRepository.getUserRole(it.uid) }
+                        _uiState.value = _uiState.value.copy(
+                            user = user,
+                            role = role,
+                            isLoading = false
+                        )
                     }
-
                     is RootResult.Error -> {
-                        Log.d("LoginViewModel", "Error: ${result.message}")
-                        _uiState.value =
-                            _uiState.value.copy(error = result.message, isLoading = false)
+                        _uiState.value = _uiState.value.copy(
+                            error = result.message,
+                            isLoading = false
+                        )
                     }
-
                     RootResult.Loading -> {
-                        Log.d("LoginViewModel", "Loading")
                         _uiState.value = _uiState.value.copy(isLoading = true)
                     }
                 }
             }
         }
     }
-
 
     internal fun signOut() {
         viewModelScope.launch {
@@ -83,23 +87,21 @@ class LoginViewModel @Inject constructor(
         }
     }
 
-
-    internal fun isLoggedIn() {
+    fun isLoggedIn() {
         _loggingState.value = _loggingState.value.copy(isLoading = true)
         viewModelScope.launch {
             isLoggedInUseCase().collect { isLoggedIn ->
                 when (isLoggedIn) {
                     is RootResult.Success -> {
-
-                        val currentUser = FirebaseAuth.getInstance().currentUser
-                        val isAnonymous = currentUser?.isAnonymous ?: false
+                        val role =
+                            authRepository.getUserRole(FirebaseAuth.getInstance().currentUser!!.uid)
 
                         _loggingState.value = _loggingState.value.copy(
                             isLoading = false,
                             transaction = isLoggedIn.data ?: false,
-                            isAnonymous = isAnonymous
+                            data = role
                         )
-                        Log.d("LoginViewModel", "Is Logged In: $isAnonymous")
+                        Log.d("LoginViewModel", "User Role: $role")
                     }
 
                     is RootResult.Error -> {
