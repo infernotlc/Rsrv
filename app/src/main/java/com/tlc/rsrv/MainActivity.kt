9b710058610e53cf1ddca8947f0575650db66183
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-18 23:52:48 +0300

    feat:edited

--- app/src/main/java/com/tlc/rsrv/MainActivity.kt
+++ app/src/main/java/com/tlc/rsrv/MainActivity.kt
@@ -4,39 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.tlc.rsrv.ui.theme.RsrvTheme
+import androidx.navigation.compose.rememberNavController
+import com.tlc.feature.feature.main_content.MainScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RsrvTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+           val navController = rememberNavController()
+            MainScreen(navController = navController)
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
