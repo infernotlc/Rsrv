beb53838cb508ffeda19d4110ea5f35126211920
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-20 02:11:03 +0300

    fix:loading stuck has been resolved implemented

--- feature/src/main/java/com/tlc/feature/feature/auth/is_logged_in/IsLoggedInScreen.kt
+++ feature/src/main/java/com/tlc/feature/feature/auth/is_logged_in/IsLoggedInScreen.kt
@@ -1,14 +1,20 @@
 package com.tlc.feature.feature.auth.is_logged_in
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.tlc.domain.utils.RootResult
@@ -18,16 +24,12 @@ import com.tlc.feature.feature.component.LoadingLottie
 import com.tlc.feature.navigation.NavigationGraph
 
 @Composable
-fun IsLoggedIn(navController: NavController, viewModel: LoginViewModel = hiltViewModel()){
+fun IsLoggedIn(navController: NavController, viewModel: LoginViewModel = hiltViewModel()) {
 
     val loggingState by viewModel.loggingState.collectAsState()
 
-    LaunchedEffect(true) {
-        viewModel.isLoggedIn()
-    }
-
     LaunchedEffect(loggingState.transaction) {
-        if (loggingState.transaction) {
+        if (!loggingState.isLoading) {
             when (loggingState.data) {
                 "admin" -> navController.navigate(NavigationGraph.ADMIN_SCREEN.route) {
                     popUpTo(NavigationGraph.ADMIN_SCREEN.route) {
@@ -50,8 +52,7 @@ fun IsLoggedIn(navController: NavController, viewModel: LoginViewModel = hiltVie
         }
     }
 
-
-    if (loggingState.isLoading){
+    if (loggingState.isLoading) {
         Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -60,5 +61,4 @@ fun IsLoggedIn(navController: NavController, viewModel: LoginViewModel = hiltVie
             LoadingLottie(R.raw.loading_lottie)
         }
     }
-
 }
