13f93aa7bffe66e1a1a8abe932b34912dfa2bae5
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-20 02:11:36 +0300

    fix:toast added

--- feature/src/main/java/com/tlc/feature/feature/customer/CustomerScreen.kt
+++ feature/src/main/java/com/tlc/feature/feature/customer/CustomerScreen.kt
@@ -1,12 +1,15 @@
 package com.tlc.feature.feature.customer
 
+import android.widget.Toast
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 
 @Composable
 fun CustomerScreen(
     navController: NavController
 ) {
-    Text(text = "Customer Screen")
+    Toast.makeText(LocalContext.current, "Customer Screen", Toast.LENGTH_SHORT).show()
 }
