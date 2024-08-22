878ea50577ee6a747ace5194bbdddc22f7ac7a8c
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-23 02:12:40 +0300

    fix:a few name changed

--- feature/src/main/java/com/tlc/feature/navigation/RsrvNavigation.kt
+++ feature/src/main/java/com/tlc/feature/navigation/RsrvNavigation.kt
@@ -14,7 +14,7 @@ import androidx.navigation.compose.composable
 import com.tlc.feature.R
 import com.tlc.feature.feature.admin.AdminScreen
 import com.tlc.feature.feature.auth.forget_password.ForgotPasswordScreen
-import com.tlc.feature.feature.auth.is_logged_in.IsLoggedIn
+import com.tlc.feature.feature.auth.is_logged_in.IsLoggedInScreen
 import com.tlc.feature.feature.auth.login.LoginScreen
 import com.tlc.feature.feature.auth.register.RegisterScreen
 import com.tlc.feature.feature.component.LoadingLottie
@@ -27,52 +27,51 @@ fun RsrvNavigation(
     onTitleChange: (String) -> Unit,
     key: Int
 ) {
-
-        LaunchedEffect(mainViewModel.startDestination) {
-            if (!mainViewModel.isLoading) {
-                navController.navigate(mainViewModel.startDestination) {
-                    popUpTo(navController.graph.startDestinationId) {
-                        inclusive = true
-                    }
-                }
-            }
-        }
-
-        if (mainViewModel.isLoading) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                LoadingLottie(resId = R.raw.loading_lottie)
-            }
-        } else {
-            NavHost(
-                navController = navController,
-                startDestination = mainViewModel.startDestination
-            ) {
-                composable(NavigationGraph.IS_LOGGED_IN.route) {
-                    IsLoggedIn(navController)
-                }
-                composable(NavigationGraph.LOGIN.route) {
-                    LoginScreen(navController)
-                    onTitleChange("Login Screen")
-                }
-                composable(NavigationGraph.REGISTER.route) {
-                    RegisterScreen(navController)
-                    onTitleChange("Register Screen")
-                }
-                composable(NavigationGraph.FORGOT_PASSWORD.route) {
-                    ForgotPasswordScreen(navController)
-                    onTitleChange("Forget Password Screen")
-                }
-                composable(NavigationGraph.ADMIN_SCREEN.route) {
-                    AdminScreen(navController)
-                    onTitleChange("Admin Screen")
-                }
-                composable(NavigationGraph.CUSTOMER_SCREEN.route) {
-                    CustomerScreen(navController)
-                    onTitleChange("Customer Screen")
+    LaunchedEffect(mainViewModel.startDestination) {
+        if (!mainViewModel.isLoading) {
+            navController.navigate(mainViewModel.startDestination) {
+                popUpTo(navController.graph.startDestinationId) {
+                    inclusive = true
                 }
             }
         }
     }
+
+    if (mainViewModel.isLoading) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            LoadingLottie(resId = R.raw.loading_lottie)
+        }
+    } else {
+        NavHost(
+            navController = navController,
+            startDestination = mainViewModel.startDestination
+        ) {
+            composable(NavigationGraph.IS_LOGGED_IN.route) {
+                IsLoggedInScreen(navController)
+            }
+            composable(NavigationGraph.LOGIN.route) {
+                LoginScreen(navController)
+                onTitleChange("Login Screen")
+            }
+            composable(NavigationGraph.REGISTER.route) {
+                RegisterScreen(navController)
+                onTitleChange("Register Screen")
+            }
+            composable(NavigationGraph.FORGOT_PASSWORD.route) {
+                ForgotPasswordScreen(navController)
+                onTitleChange("Forget Password Screen")
+            }
+            composable(NavigationGraph.ADMIN_SCREEN.route) {
+                AdminScreen(navController)
+                onTitleChange("Admin Screen")
+            }
+            composable(NavigationGraph.CUSTOMER_SCREEN.route) {
+                CustomerScreen(navController)
+                onTitleChange("Customer Screen")
+            }
+        }
+    }
+}
