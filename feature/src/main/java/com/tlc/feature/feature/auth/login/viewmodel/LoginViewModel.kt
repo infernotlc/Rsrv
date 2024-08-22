faa03dc26790617dbed3c3b9b29ffb80da84c166
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-23 02:11:45 +0300

    fix:so much things tried in the end init solved all the problem ı never forget what init does

--- feature/src/main/java/com/tlc/feature/feature/auth/login/viewmodel/LoginViewModel.kt
+++ feature/src/main/java/com/tlc/feature/feature/auth/login/viewmodel/LoginViewModel.kt
@@ -1,15 +1,19 @@
 package com.tlc.feature.feature.auth.login.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.tlc.data.ui.repository.firebase.AuthRepositoryImpl
+import com.tlc.domain.repository.firebase.AuthRepository
 import com.tlc.domain.use_cases.firebase_use_cases.auth.IsLoggedInUseCase
 import com.tlc.domain.use_cases.firebase_use_cases.auth.SignInUseCase
 import com.tlc.domain.use_cases.firebase_use_cases.auth.SignOutUseCase
 import com.tlc.domain.utils.RootResult
 import com.tlc.feature.feature.auth.login.state.IsLoggedInState
 import com.tlc.feature.feature.auth.login.state.LoginUiState
+import com.tlc.feature.navigation.main_datastore.MainDataStore
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -21,7 +25,7 @@ class LoginViewModel @Inject constructor(
     private val signInUseCase: SignInUseCase,
     private val signOutUseCase: SignOutUseCase,
     private val isLoggedInUseCase: IsLoggedInUseCase,
-    private val authRepository: AuthRepositoryImpl
+    private val authRepository: AuthRepository
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(LoginUiState())
@@ -30,6 +34,9 @@ class LoginViewModel @Inject constructor(
     private val _loggingState = MutableStateFlow(IsLoggedInState())
     val loggingState: StateFlow<IsLoggedInState> = _loggingState
 
+    init {
+        isLoggedIn()
+    }
 
     internal fun signIn(email: String, password: String) {
         viewModelScope.launch {
