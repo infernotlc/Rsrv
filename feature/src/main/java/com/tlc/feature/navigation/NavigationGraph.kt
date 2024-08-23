76a3b10b223c1c7c19cba35142bf712b1ae3e8cb
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-24 02:55:49 +0300

    feat:nav graph updated

--- feature/src/main/java/com/tlc/feature/navigation/NavigationGraph.kt
+++ feature/src/main/java/com/tlc/feature/navigation/NavigationGraph.kt
@@ -1,10 +1,23 @@
 package com.tlc.feature.navigation
 
+import com.google.gson.Gson
+import com.tlc.data.remote.dto.firebase_dto.PlaceDataDto
+
 enum class NavigationGraph(val route: String) {
     IS_LOGGED_IN("is_logged_in"),
     LOGIN("login"),
     REGISTER("register"),
     FORGOT_PASSWORD("forgot_password"),
     ADMIN_SCREEN("admin_screen"),
+    DESIGN_SCREEN("design_screen/{placeJson}"),
     CUSTOMER_SCREEN("customer_screen");
+
+
+    companion object {
+        fun getDesignRoute(place: PlaceDataDto): String {
+            val gson = Gson()
+            val placeJson = gson.toJson(place)
+            return "design_screen/$placeJson"
+        }
+    }
 }
