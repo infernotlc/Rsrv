379b377f65d2a13ce19b40fa07f099af584a68c6
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-23 21:35:30 +0300

    feat:new fun added for reservation

--- data/src/main/java/com/tlc/data/ui/di/FirebaseModule.kt
+++ data/src/main/java/com/tlc/data/ui/di/FirebaseModule.kt
@@ -3,7 +3,9 @@ package com.tlc.data.ui.di
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.tlc.data.ui.repository.firebase.AuthRepositoryImpl
+import com.tlc.data.ui.repository.firebase.ReservationRepositoryImpl
 import com.tlc.domain.repository.firebase.AuthRepository
+import com.tlc.domain.repository.firebase.ReservationRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -33,5 +35,15 @@ object FirebaseModule {
         firestore: FirebaseFirestore
     ): AuthRepository {
         return AuthRepositoryImpl(firebaseAuth, firestore)
+    }
+
+    @Provides
+    @Singleton
+    fun provideReservationRepository(
+        firebaseAuth: FirebaseAuth,
+        firestore: FirebaseFirestore
+    ): ReservationRepository {
+        return ReservationRepositoryImpl(firebaseAuth,firestore)
+
     }
 }
