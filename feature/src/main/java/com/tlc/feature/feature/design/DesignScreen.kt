864d902ec87281d6533456450e486920628cab2e
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-24 02:55:22 +0300

    feat:design screen created for test

---
+++ feature/src/main/java/com/tlc/feature/feature/design/DesignScreen.kt
@@ -0,0 +1,10 @@
+package com.tlc.feature.feature.design
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.tlc.domain.model.firebase.PlaceData
+
+@Composable
+fun DesignScreen(navController: NavController, place: PlaceData) {
+
+}
