15489434664bc372d20c6003e996a648aad7ece3
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-24 20:41:00 +0300

    feat:load use-case added !! todo

---
+++ domain/src/main/java/com/tlc/domain/use_cases/design/LoadDesignUseCase.kt
@@ -0,0 +1,11 @@
+package com.tlc.domain.use_cases.design
+
+import com.tlc.domain.repository.firebase.DesignRepository
+import javax.inject.Inject
+
+class LoadDesignUseCase @Inject constructor(
+    private val designRepository: DesignRepository
+) {
+    suspend fun loadDesign(placeId: String) = designRepository.getDesign(placeId)
+
+}
