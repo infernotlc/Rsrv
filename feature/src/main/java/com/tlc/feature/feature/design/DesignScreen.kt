3cc8c7c68d4a505712d219161a647a01cb224ef9
Talha <70065773+infernotlc@users.noreply.github.com>
2024-08-24 20:46:14 +0300

    feat:in parameters model type changed

--- feature/src/main/java/com/tlc/feature/feature/design/DesignScreen.kt
+++ feature/src/main/java/com/tlc/feature/feature/design/DesignScreen.kt
@@ -2,9 +2,10 @@ package com.tlc.feature.feature.design
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import com.tlc.data.remote.dto.firebase_dto.Place
 import com.tlc.domain.model.firebase.PlaceData
 
 @Composable
-fun DesignScreen(navController: NavController, place: PlaceData) {
+fun DesignScreen(navController: NavController, place: Place) {
 
 }
