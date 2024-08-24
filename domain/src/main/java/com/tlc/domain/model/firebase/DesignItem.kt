851ab05d070b0407ed024c572a43b1a383aa01ab
Talha Çalışır <70065773+infernotlc@users.noreply.github.com>
2024-08-25 00:00:00 +0300

    fix:to serialize from database need to create a constructor with empty values, or give default values

--- domain/src/main/java/com/tlc/domain/model/firebase/DesignItem.kt
+++ domain/src/main/java/com/tlc/domain/model/firebase/DesignItem.kt
@@ -4,7 +4,7 @@ import java.util.UUID
 
 data class DesignItem(
     val id: String = UUID.randomUUID().toString(),
-    val type: String,
-    var xPosition: Float,
-    var yPosition: Float
+    val type: String = "",
+    var xPosition: Float = 0f,
+    var yPosition: Float = 0f
 )
