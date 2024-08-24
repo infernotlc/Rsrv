d27c7f0d9feca5be43216ac0445906223da57397
Talha Çalışır <70065773+infernotlc@users.noreply.github.com>
2024-08-25 02:57:06 +0300

    feat: in design area boundries added dynamically. No matter which phone is be we use phone width and height edges

--- feature/src/main/java/com/tlc/feature/feature/design/DesignScreen.kt
+++ feature/src/main/java/com/tlc/feature/feature/design/DesignScreen.kt
@@ -1,15 +1,13 @@
 package com.tlc.feature.feature.design
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
@@ -21,13 +19,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.tlc.data.remote.dto.firebase_dto.Place
 import com.tlc.domain.model.firebase.DesignItem
 import com.tlc.feature.feature.design.viewmodel.DesignViewModel
+
 
 @Composable
 fun DesignScreen(
@@ -37,91 +39,107 @@ fun DesignScreen(
     placeId: String
 ) {
     val uiState by viewModel.uiState.collectAsState()
-    val saveState by viewModel.saveState.collectAsState()
 
     // Local state for design items
     val designItems = remember { mutableStateListOf<DesignItem>() }
 
-    // Load design items when the screen is first loaded
-    LaunchedEffect(placeId) {
-        viewModel.loadDesign(placeId)
-    }
-
-    // Update local designItems when UI state changes
-    LaunchedEffect(uiState.designItems) {
-        if (uiState.designItems != null) {
-            designItems.clear()
-            designItems.addAll(uiState.designItems)
-        }
-    }
-
-    Column(
+    BoxWithConstraints(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
     ) {
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Button(onClick = {
-                // Add a new table only if it doesn't already exist
-                val newTable = DesignItem(
-                    type = "TABLE",
-                    xPosition = 50f,
-                    yPosition = 50f
-                )
-                if (designItems.none { it.type == "TABLE" && it.xPosition == 50f && it.yPosition == 50f }) {
-                    designItems.add(newTable)
-                }
-            }) {
-                Text("Add Table")
+        val density = LocalDensity.current
+        val screenWidthPx = with(density) { maxWidth.toPx() }
+        val screenHeightPx = with(density) { maxHeight.toPx() }
+
+        // Calculate boundary dynamically
+        val boundary = remember {
+            Rect(
+                left = 0f,
+                top = 10f,
+                right = screenWidthPx,
+                bottom = screenHeightPx - 150f
+            )
+        }
+
+        // Load design items when the screen is first loaded
+        LaunchedEffect(placeId) {
+            viewModel.loadDesign(placeId)
+        }
+
+        // Update local designItems when UI state changes
+        LaunchedEffect(uiState.designItems) {
+            if (uiState.designItems != null) {
+                designItems.clear()
+                designItems.addAll(uiState.designItems)
             }
-            Button(
-                onClick = {
-                    viewModel.saveDesign(placeId, designItems)
-                },
+        }
+
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
             ) {
-                Text("Save Design")
-            }
-
-            Button(onClick = {
-                val newChair = DesignItem(
-                    type = "CHAIR",
-                    xPosition = 50f,
-                    yPosition = 50f
-                )
-                if (designItems.none { it.type == "CHAIR" && it.xPosition == 50f && it.yPosition == 50f }) {
-                    designItems.add(newChair)
-                }
-            }) {
-                Text("Add Chair")
-            }
-        }
-
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.LightGray)
-                .padding(8.dp)
-        ) {
-            designItems.forEach { item ->
-                DraggableItem(
-                    item = item,
-                    onPositionChange = { updatedPosition ->
-                        item.xPosition = updatedPosition.x
-                        item.yPosition = updatedPosition.y
+                Button(onClick = {
+                    // Add a new table only if it doesn't already exist
+                    val newTable = DesignItem(
+                        type = "TABLE",
+                        xPosition = 50f,
+                        yPosition = 50f
+                    )
+                    if (designItems.none { it.type == "TABLE" && it.xPosition == 50f && it.yPosition == 50f }) {
+                        designItems.add(newTable)
                     }
-                )
+                }) {
+                    Text("Add Table")
+                }
+                Button(
+                    onClick = {
+                        viewModel.saveDesign(placeId, designItems)
+                    },
+                ) {
+                    Text("Save Design")
+                }
+
+                Button(onClick = {
+                    val newChair = DesignItem(
+                        type = "CHAIR",
+                        xPosition = 50f,
+                        yPosition = 50f
+                    )
+                    if (designItems.none { it.type == "CHAIR" && it.xPosition == 50f && it.yPosition == 50f }) {
+                        designItems.add(newChair)
+                    }
+                }) {
+                    Text("Add Chair")
+                }
             }
-        }
 
-        if (uiState.isLoading) {
-            CircularProgressIndicator()
-        }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.LightGray)
+            ) {
+                designItems.forEach { item ->
+                    DraggableItem(
+                        item = item,
+                        boundary = boundary,
+                        onPositionChange = { updatedPosition ->
+                            item.xPosition = updatedPosition.x
+                            item.yPosition = updatedPosition.y
+                        }
+                    )
+                }
+            }
 
-        uiState.error?.let {
-            Text("Error: $it", color = Color.Red)
+            if (uiState.isLoading) {
+                CircularProgressIndicator()
+            }
+
+            uiState.error?.let {
+                Text("Error: $it", color = Color.Red)
+            }
         }
     }
 }
