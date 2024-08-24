41859722f2226c08479669685eb7fb8e393a8c2a
Talha Çalışır <70065773+infernotlc@users.noreply.github.com>
2024-08-25 00:01:59 +0300

    fix:commited to be sure

--- feature/src/main/java/com/tlc/feature/feature/design/state/DesignState.kt
+++ feature/src/main/java/com/tlc/feature/feature/design/state/DesignState.kt
@@ -2,8 +2,8 @@ package com.tlc.feature.feature.design.state
 
 import com.tlc.domain.model.firebase.DesignItem
 
-data class DesignState(
-    val designItems: List<DesignItem> = emptyList(),
-    val isLoading: Boolean = false,
-    val error: String? = null
-)
+    data class DesignState(
+        val designItems: List<DesignItem> = emptyList(),
+        val isLoading: Boolean = false,
+        val error: String? = null
+    )
