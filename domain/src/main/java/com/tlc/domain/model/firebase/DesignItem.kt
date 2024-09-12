78e71bc8feec8341bb6b5e6bf1804c4f7d2dd427
Talha <70065773+infernotlc@users.noreply.github.com>
2024-09-13 00:44:33 +0300

    feat

--- domain/src/main/java/com/tlc/domain/model/firebase/DesignItem.kt
+++ domain/src/main/java/com/tlc/domain/model/firebase/DesignItem.kt
@@ -1,10 +1,15 @@
 package com.tlc.domain.model.firebase
 
+import com.google.firebase.Timestamp
 import java.util.UUID
 
 data class DesignItem(
     val designId: String = UUID.randomUUID().toString(),
     val type: String = "",
     var xPosition: Float = 0f,
-    var yPosition: Float = 0f
+    var yPosition: Float = 0f,
+    var isReserved: Boolean = false,
+    var reservedBy: String? = "",
+    var reservationStartTime: Timestamp?= null,
+    var reservationEndTime: Timestamp? = null
 )
