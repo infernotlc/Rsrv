5b3e46fa12f6d9eb33a5e1db62c01c6fa2351e12
infernotlc <talha5797@gmail.com>
2025-02-01 00:04:54 +0300

    feat:updated new repo

--- domain/src/main/java/com/tlc/domain/repository/firebase/ReservationRepository.kt
+++ domain/src/main/java/com/tlc/domain/repository/firebase/ReservationRepository.kt
@@ -1,19 +1,12 @@
 package com.tlc.domain.repository.firebase
 
-import com.google.firebase.Timestamp
 import com.tlc.domain.model.firebase.Reservation
 
 interface ReservationRepository {
     suspend fun saveReservation(
-        chairId: String,
-        tableId: String,
-        customerId: String,
-        date: String,
-        time: String,
         placeId: String,
-        isApproved: Boolean,
-        timestamp: Timestamp
-    )
+        reservations: List<Reservation>
+    ):Result<Unit>
 
     suspend fun cancelUnapprovedReservations(placeId: String)
 
