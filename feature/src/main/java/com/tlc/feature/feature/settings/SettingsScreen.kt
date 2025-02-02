92eb07e83cb9897be580d91ab23cf7b1511c8dba
infernotlc <talha5797@gmail.com>
2025-02-02 23:55:05 +0300

    feat: new screen

---
+++ feature/src/main/java/com/tlc/feature/feature/settings/SettingsScreen.kt
@@ -0,0 +1,9 @@
+package com.tlc.feature.feature.settings
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun SettingsScreen(navController: NavHostController) {
+
+}
