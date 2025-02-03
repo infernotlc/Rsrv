0a57cdd673f299f72beaaacf605853a1f14df061
infernotlc <talha5797@gmail.com>
2025-02-03 22:16:40 +0300

    feat:

--- domain/src/main/java/com/tlc/domain/model/firebase/Reservation.kt
+++ domain/src/main/java/com/tlc/domain/model/firebase/Reservation.kt
@@ -3,13 +3,14 @@ package com.tlc.domain.model.firebase
 import com.google.firebase.Timestamp
 
 data class Reservation(
-    val chairId: String = "",
+    val tableId: String = "",
     val holderName: String = "",
     val holderPhoneNo: String = "",
     val customerCount: Int = 0,
     val animalCount: Int = 0,
     val date: String = "",
     val time: String = "",
+    val isReserved: Boolean = true,
     val isApproved: Boolean = false,
     val timestamp: Timestamp = Timestamp.now()
 
