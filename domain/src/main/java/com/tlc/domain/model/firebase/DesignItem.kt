d61bdfc596424e18f5df7944efcd70c32edbf9b9
infernotlc <talha5797@gmail.com>
2025-02-09 21:03:44 +0300

    feat: reserved logic transferred to DesignItem cause of Table could totally reserved

--- domain/src/main/java/com/tlc/domain/model/firebase/DesignItem.kt
+++ domain/src/main/java/com/tlc/domain/model/firebase/DesignItem.kt
@@ -1,10 +1,14 @@
 package com.tlc.domain.model.firebase
 
+import com.google.firebase.firestore.PropertyName
 import java.util.UUID
 
 data class DesignItem(
     val designId: String = UUID.randomUUID().toString(),
     val type: String = "",
     var xPosition: Float = 0f,
-    var yPosition: Float = 0f
+    var yPosition: Float = 0f,
+
+    @get:PropertyName("reserved") @set:PropertyName("reserved")
+    var isReserved: Boolean = false,
 )
