72edf7de53bc97cc180e34efa2f6bcecc56e69eb
infernotlc <talha5797@gmail.com>
2025-02-11 22:15:50 +0300

    feat: logout added and working

--- feature/src/main/java/com/tlc/feature/feature/profile/ProfileScreen.kt
+++ feature/src/main/java/com/tlc/feature/feature/profile/ProfileScreen.kt
@@ -1,9 +1,46 @@
 package com.tlc.feature.feature.profile
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import androidx.wear.compose.material.Text
+import com.tlc.feature.feature.auth.login.viewmodel.LoginViewModel
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun ProfileScreen(navController: NavHostController) {
+fun ProfileScreen(navController: NavHostController, onLogout: () -> Unit) {
 
+    Scaffold(modifier = Modifier.fillMaxWidth(),
+        containerColor = Color.White
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+
+        ) {
+            Button(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally), onClick = {
+                    onLogout()
+                },
+                shape = MaterialTheme.shapes.medium,
+                colors = ButtonDefaults.buttonColors(Color.Black)
+            ) {
+                Text("Sign Out")
+            }
+
+
+        }
+    }
 }
+
