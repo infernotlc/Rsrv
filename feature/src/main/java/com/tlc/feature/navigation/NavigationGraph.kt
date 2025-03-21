f8322aabba71c10fef41b387449c8dde7f8fc4c8
infernotlc <talha5797@gmail.com>
2025-03-21 23:22:11 +0300

    topbar and navigation bar logic seperated

--- feature/src/main/java/com/tlc/feature/navigation/NavigationGraph.kt
+++ feature/src/main/java/com/tlc/feature/navigation/NavigationGraph.kt
@@ -23,5 +23,25 @@ enum class NavigationGraph(val route: String) {
             val encodedPlaceJson = URLEncoder.encode(placeJson, StandardCharsets.UTF_8.toString())
             return "design_screen/$encodedPlaceJson"
         }
+
+        // For showing/hiding the navigation drawer
+        fun shouldShowNavigationDrawer(route: String): Boolean {
+            return route == ADMIN_SCREEN.route || route == CUSTOMER_SCREEN.route
+        }
+
+        // For showing/hiding the top bar
+        fun shouldShowTopBar(route: String): Boolean {
+            return when {
+                route == CUSTOMER_SCREEN.route -> true
+                route == ADMIN_SCREEN.route -> true
+                route == LOGIN.route -> true
+                route == REGISTER.route -> true
+                route == FORGOT_PASSWORD.route -> true
+                route == PROFILE_SCREEN.route -> true
+                route.startsWith("design_screen/") -> true
+                route.startsWith("save_reservation_screen/") -> true
+                else -> false
+            }
+        }
     }
 }
