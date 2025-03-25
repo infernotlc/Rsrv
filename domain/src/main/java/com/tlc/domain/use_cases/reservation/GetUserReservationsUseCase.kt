f21e8211b187db283cabaa2d2981f247f5ad0180
infernotlc <talha5797@gmail.com>
2025-03-26 01:31:43 +0300

    rsrv is saving in customer folder also

--- domain/src/main/java/com/tlc/domain/use_cases/reservation/GetUserReservationsUseCase.kt
+++ domain/src/main/java/com/tlc/domain/use_cases/reservation/GetUserReservationsUseCase.kt
@@ -3,12 +3,13 @@ package com.tlc.domain.use_cases.reservation
 import com.tlc.domain.model.firebase.Reservation
 import com.tlc.domain.repository.firebase.ReservationRepository
 import com.tlc.domain.utils.RootResult
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetUserReservationsUseCase @Inject constructor(
     private val repository: ReservationRepository
 ) {
-    suspend operator fun invoke(userId: String): RootResult<List<Reservation>> {
+    suspend operator fun invoke(userId: String): Flow<List<Reservation>> {
         return repository.getUserReservations(userId)
     }
 } 
