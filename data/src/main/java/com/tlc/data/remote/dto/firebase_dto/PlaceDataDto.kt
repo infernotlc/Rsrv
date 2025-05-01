bfe919fe79bc56e9af548ac890a9bb91f2580343
infernotlc <talha5797@gmail.com>
2025-05-01 20:56:54 +0300

    feat: new values added

--- data/src/main/java/com/tlc/data/remote/dto/firebase_dto/PlaceDataDto.kt
+++ data/src/main/java/com/tlc/data/remote/dto/firebase_dto/PlaceDataDto.kt
@@ -8,6 +8,7 @@ data class PlaceDataDto(
     val name: String="",
     val placeImageUrl: String="",
     val capacity: Int=0,
-    val reservationTimes: List<String> = emptyList()
-
+    val reservationTimes: List<String> = emptyList(),
+    val country: String = "",
+    val city: String = ""
 )
