a843db561e11270cd57344c6ca2262072b365523
infernotlc <talha5797@gmail.com>
2025-06-22 22:25:01 +0300

    feat: useless usecases deleted

--- domain/src/main/java/com/tlc/domain/use_cases/reservation/CancelUnapprovedReservationsUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.tlc.domain.use_cases.reservation
-
-import com.tlc.domain.repository.firebase.ReservationRepository
-import javax.inject.Inject
-
-class CancelUnapprovedReservationsUseCase @Inject constructor(
-    private val reservationRepository: ReservationRepository) {
-        suspend operator fun invoke(placeId : String) = reservationRepository.cancelUnapprovedReservations(placeId)
-    }
